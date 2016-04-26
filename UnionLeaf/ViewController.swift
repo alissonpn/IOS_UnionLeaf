0b717c48058fc5d69522bfff4076d1a9ff7ea1c8
student01 <student@student01.com>
2016-04-26 15:56:12 -0300

    initial commit

---
+++ UnionLeaf/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UnionLeaf
+//
+//  Created by Student on 4/26/16.
+//  Copyright © 2016 HackaTruck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("teste 123")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
